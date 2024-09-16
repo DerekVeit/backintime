36a5c50e239cf1be6a7c35c9959de280357f972f
Derek Veit <derek.veit>
2024-09-16 10:53:15 -0700

    test_rsync_selections: combine params_for_cases and params_for_raise_cases.

--- common/test/test_rsync_selections.py
+++ common/test/test_rsync_selections.py
@@ -43,47 +43,25 @@ def params_for_cases(cases_file):
             )
         )
 
+        if "expected_tree" in specs:
+            expected = filetree.normal(specs["expected_tree"])
+        elif "raises" in specs:
+            expected_exception, expected_message = specs["raises"].split(None, 1)
+            # Replace the string, e.g. "ValueError", with what it names.
+            expected_exception = {**__builtins__, **globals()}[expected_exception]
+            expected_message = expected_message.strip()
+            expected = expected_exception, expected_message
+
         params.append(
             pytest.param(
                 textwrap.dedent(specs["includes"]).splitlines(),
                 textwrap.dedent(specs["excludes"]).splitlines(),
                 filetree.normal(specs["files_tree"]),
-                filetree.normal(specs["expected_tree"]),
+                expected,
                 id=case_name,
             )
         )
 
-    return params
-
-
-def params_for_raise_cases(cases_file):
-    cases = (pathlib.Path(__file__).parent / cases_file).read_text()
-
-    params = []
-    for case in (c for c in cases.split("\n:") if c.strip()):
-        case_name, rest = case.split("\n", 1)
-        if "SKIP" in case_name:
-            continue
-        rest = "".join(line.split("#")[0].rstrip() + "\n" for line in rest.splitlines())
-        specs = dict(
-            re.findall(
-                r"(\w+)\n((?: .*\n)*)",
-                textwrap.dedent(rest),
-            )
-        )
-        expected_exception, expected_message = specs["raises"].split(None, 1)
-        expected_exception = {**__builtins__, **globals()}[expected_exception]
-        expected_message = expected_message.strip()
-        params.append(
-            pytest.param(
-                textwrap.dedent(specs["includes"]).splitlines(),
-                textwrap.dedent(specs["excludes"]).splitlines(),
-                filetree.normal(specs["files_tree"]),
-                expected_exception,
-                expected_message,
-                id=case_name,
-            )
-        )
     return params
 
 
@@ -129,13 +107,15 @@ def test_rsyncSuffix(
 
 
 @pytest.mark.parametrize(
-    "includes, excludes, files_tree, expected_exception, expected_message",
-    params_for_raise_cases("selection_raise_cases"),
+    "includes, excludes, files_tree, expected",
+    params_for_cases("selection_raise_cases"),
 )
 def test_rsyncSuffix__raises(
-    includes, excludes, files_tree, expected_exception, expected_message, tmp_path, bit_snapshot
+    includes, excludes, files_tree, expected, tmp_path, bit_snapshot
 ):
     includes, excludes = prepend_paths(tmp_path, includes, excludes)
+
+    expected_exception, expected_message = expected
 
     log("\n  ".join(p for p in includes))
     log("\n  ".join(p for p in excludes))
@@ -148,7 +128,7 @@ def test_rsyncSuffix__raises(
     bit_snapshot.config.setExclude(bit_snapshot.config.exclude() + excludes)
     log(f"{bit_snapshot.config.exclude() = }")
 
-    with pytest.raises(ValueError, match=expected_message + ":.*"):
+    with pytest.raises(expected_exception, match=expected_message + ":.*"):
         bit_snapshot.backup()
 
 

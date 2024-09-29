e00205442dc3f832ef6ba23b6acbd33b621d1853
Derek Veit <derek.veit>
2024-09-29 13:11:33 -0700

    conftest, test_fixtures: add type hints.

--- common/test/conftest.py
+++ common/test/conftest.py
@@ -1,3 +1,4 @@
+from collections.abc import Generator
 from pathlib import Path
 
 import pytest
@@ -8,30 +9,30 @@ import tools
 
 
 @pytest.fixture
-def bit_config(tmp_path):
+def bit_config(tmp_path: Path) -> Generator[config.Config, None, None]:
     config_path = (Path(__file__) / "../config").resolve()
     data_path = tmp_path / "data"
 
     bit_config = config.Config(
         str(config_path),   # config file at backintime/common/test/config
         str(data_path),     # real default would be ~/.local/share
-    )
+    )  # type: ignore[no-untyped-call]
 
     snapshots_path = tmp_path / "snapshots"
     snapshots_path.mkdir()
-    bit_config.set_snapshots_path(str(snapshots_path))
+    bit_config.set_snapshots_path(str(snapshots_path))  # type: ignore[no-untyped-call]
     tools.validate_and_prepare_snapshots_path(
         path=snapshots_path,
-        host_user_profile=bit_config.hostUserProfile(),
-        mode=bit_config.snapshotsMode(),
-        copy_links=bit_config.copyLinks(),
+        host_user_profile=bit_config.hostUserProfile(),  # type: ignore[no-untyped-call]
+        mode=bit_config.snapshotsMode(),  # type: ignore[no-untyped-call]
+        copy_links=bit_config.copyLinks(),  # type: ignore[no-untyped-call]
         error_handler=bit_config.notifyError)
 
-    bit_config.SELECTIONS_MODE = "sorted"
+    bit_config.SELECTIONS_MODE = "sorted"  # type: ignore[attr-defined]
 
     yield bit_config
 
 
 @pytest.fixture
-def bit_snapshot(bit_config):
-    yield snapshots.Snapshots(bit_config)
+def bit_snapshot(bit_config: config.Config) -> Generator[snapshots.Snapshots, None, None]:
+    yield snapshots.Snapshots(bit_config)  # type: ignore[no-untyped-call]
--- common/test/test_fixtures.py
+++ common/test/test_fixtures.py
@@ -4,14 +4,14 @@ import snapshots
 from test.logging import log
 
 
-def test_config(bit_config):
+def test_config(bit_config: config.Config) -> None:
     log(f"{bit_config._LOCAL_MOUNT_ROOT = }")
     assert isinstance(bit_config, config.Config)
-    excludes = bit_config.exclude()
+    excludes = bit_config.exclude()  # type: ignore[no-untyped-call]
     assert ".Private" in excludes
     assert "/home/derek/Local" not in excludes
 
 
-def test_snapshot(bit_snapshot):
+def test_snapshot(bit_snapshot: snapshots.Snapshots) -> None:
     log(f"{bit_snapshot.config._LOCAL_MOUNT_ROOT = }")
     assert isinstance(bit_snapshot, snapshots.Snapshots)
